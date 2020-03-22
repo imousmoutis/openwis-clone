625003015eb3f77d972052b842fe40b8ff8354ae
Ioannis Mousmoutis <Ioannis.Mousmoutis@eurodyn.com>
2020-03-22 02:15:36 +0200

    fixed url

--- maven_release.sh
+++ maven_release.sh
@@ -6,5 +6,5 @@ mvn --batch-mode release:prepare
 git fetch --all
 git checkout -b develop origin/develop
 git merge --no-edit --no-ff master
-git remote set-url origin git@github.com:imousmoutis/opewis-clone.git
+git remote set-url origin git@github.com:imousmoutis/openwis-clone.git
 git push -u origin develop
